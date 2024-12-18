a4da6672e8f50f89145bee89e947a842bee89d1e
Rahul <rahuleetcode17@gmail.com>
2024-12-19 02:39:42 +0530

    frontend_url_change

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://localhost:3000",
+    origin: "https://academyx-edtech-app.vercel.app",
     credentials: true,
     optionSuccessStatus: 200,
   })
